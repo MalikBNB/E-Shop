42c04c1bf75a70dafab06ebf23545f7adcbe8c3c
Aymen Abdelmalik <167820077+MalikBNB@users.noreply.github.com>
2025-04-25 18:32:24 +0100

    Initial Commit

---
+++ E-Shop/Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+
+        #region DbSets
+
+        public DbSet<Product> Products { get; set; }
+
+        #endregion DbSets
+    }
+}
